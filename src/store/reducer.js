24691c26ad99e0970ec317c3a5b991b707157903
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-04-14 08:34:17 +0300

    changed reducer.js if-statements to switch cases

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,27 +3,47 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter +1
-        }
-    }
-    if(action.type === 'DECREMENT') {
-        return {
-            counter: state.counter -1
-        }
-    }
-    if(action.type === 'ADD') {
-        return {
-            counter: state.counter + action.value
-        }
-    }
-    if(action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.value
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.value
+            }
+
     }
     return state;
+    // if(action.type === 'INCREMENT') {
+    //     return {
+    //         counter: state.counter +1
+    //     }
+    // }
+    // if(action.type === 'DECREMENT') {
+    //     return {
+    //         counter: state.counter -1
+    //     }
+    // }
+    // if(action.type === 'ADD') {
+    //     return {
+    //         counter: state.counter + action.value
+    //     }
+    // }
+    // if(action.type === 'SUBTRACT') {
+    //     return {
+    //         counter: state.counter - action.value
+    //     }
+    // }
+    // return state;
 };
 
 export default reducer;
