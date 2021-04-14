1cf68f3858a07d4194c4253d6123c1b7b3e86119
Jussi-Savoranta <jussi.savoranta@gmail.com>
2021-04-14 09:04:33 +0300

    added functionality to store result

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,26 +1,35 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state); // creates new js-object and distributes old state values to it
+            newState.counter = state.counter +1;    // updating the newState
+            return newState;                        // overwriting only this property to the state
         case 'DECREMENT':
             return {
-                counter: state.counter - 1
+                ...state, // spread operator does the same exact thing as above, but is shorter way of writing it
+                counter: state.counter - 1 // this leaves state.results untouched, it doesn't overwrite that
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
             }
-
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: state.counter})
+                // concat is kind of a push. Push manipulates original array, but concat returns new array where the new value is added to old array 
+            }
     }
     return state;
     // if(action.type === 'INCREMENT') {
